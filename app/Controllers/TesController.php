43abe757560325143ea13640b11e6239f0106174
rendra-rio <rendrario272@gmail.com>
2022-05-13 08:47:55 +0700

    first commit

---
+++ app/Controllers/TesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use CodeInigter\Controller;
+
+class TesController extends Controller{
+    public function index()
+    {
+        return view('tesview');
+    }  
+ 
+}
